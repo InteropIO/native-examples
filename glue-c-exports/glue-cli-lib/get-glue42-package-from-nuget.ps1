acd8b7656160e0c91645c36f0e29e943b26ec4f5
speichev <stanislav.peichev@tick42.com>
2022-02-21 15:15:03 +0200

    glue-cli-lib + ps1 script to download Glue42 .net package

---
+++ glue-c-exports/glue-cli-lib/get-glue42-package-from-nuget.ps1
@@ -0,0 +1,7 @@
+"Downloading Glue42 .NET package"
+(new-object System.Net.WebClient).DownloadFile("https://www.nuget.org/api/v2/package/Glue42/2018.2181.0","./glue42.nupkg")
+"Unpacking Glue42 .NET package"
+tar -xf glue42.nupkg lib/net45/
+cmd /C "move /Y lib\net45\* . > nul"
+cmd /C "del *.nupkg > nul"
+cmd /C "rd /S /Q lib"
