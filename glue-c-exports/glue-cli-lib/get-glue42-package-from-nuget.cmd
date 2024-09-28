7ffab93daa0447f45ae0d60ea2e4b091cf9048d8
Stanislav Peichev <stanislav.peichev@interop.io>
2024-09-28 17:33:19 +0300

    glue-c-exports - package scripts updated to glue-cli-lib.1.5.0

--- glue-c-exports/glue-cli-lib/get-glue42-package-from-nuget.cmd
+++ glue-c-exports/glue-cli-lib/get-glue42-package-from-nuget.cmd
@@ -1,9 +1,23 @@
 @echo off
-echo Downloading Glue42 .NET package
-curl "https://globalcdn.nuget.org/packages/glue42.2018.2231.0.nupkg" --output "./glue42.nupkg"
-echo Unpacking Glue42 .NET package
-tar -xf glue42.nupkg lib/net45/
+echo Downaloding glue-cli-lib package
+curl "https://globalcdn.nuget.org/packages/glue-cli-lib.1.5.0.nupkg?packageVersion=1.5.0" --output "./glue-cli-lib.nupkg"
+echo Downloading io.Connect.NET package
+echo Unpacking glue-cli-lib package
+tar -xf glue-cli-lib.nupkg glue-cli-lib
+curl "https://globalcdn.nuget.org/packages/io.connect.net.1.22.0.nupkg" --output "./io.connect.NET.nupkg"
+echo Unpacking io.connect.NET package
+tar -xf io.connect.NET.nupkg lib/net45/
+
+echo Updating local copies
+rd /S /Q 32bit
+move /Y glue-cli-lib\32bit . > nul
+move /Y glue-cli-lib\*.dll . > nul
+move /Y glue-cli-lib\*.pdb . > nul
+move /Y glue-cli-lib\*.lib . > nul
+move /Y glue-cli-lib\*.h . > nul
 move /Y lib\net45\* . > nul
+echo Cleaning up
 del *.nupkg > nul
 rd /S /Q lib
+rd /S /Q glue-cli-lib
 echo Done
